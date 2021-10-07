400d332f1c0a25c52fc2679b2214b061ed950bae
BramHillebrand <55744510+BramHillebrand@users.noreply.github.com>
2021-10-07 09:51:21 +0200

    added a test for saving and loading

--- pySIMDEUM/tests/test_house.py
+++ pySIMDEUM/tests/test_house.py
@@ -25,3 +25,16 @@ def test_average_users():
     assert mean(number_of_users) > 2.1
     assert mean(number_of_users) < 2.5
 
+def test_save_and_load_house():
+    stats = Statistics()
+    prop = Property(statistics=stats)
+    house = prop.built_house()
+    house.populate_house()
+    house.save_house('unittest')
+
+    prop2 = Property(statistics=stats)
+    house2 = prop2.built_house(housefile='unittest.house')
+
+    assert house.id == house2.id
+
+
