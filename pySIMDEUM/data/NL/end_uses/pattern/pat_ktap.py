1fe580519838e3ffb8baababd18bba31f3c14a08
steffelbauer <david.steffelbauer@gmail.com>
2022-07-11 10:22:21 +0200

    to circumvent future warning the squeeze argument was replaced by the squeeze method

--- pySIMDEUM/data/NL/end_uses/pattern/pat_ktap.py
+++ pySIMDEUM/data/NL/end_uses/pattern/pat_ktap.py
@@ -9,7 +9,7 @@ def ktap_daily_pattern(filename=None, resolution='1s'):
         pathname = os.path.dirname(os.path.abspath(__file__))
         filename = os.path.join(pathname, 'dp_kitchen_tap.txt')
 
-    data = pd.read_csv(filename, header=None, squeeze=True).values
+    data = pd.read_csv(filename, header=None).squeeze().values
     index = pd.timedelta_range(start='00:00:00', freq='15Min', end='24:00:00')
     s = pd.Series(data=data, index=index)
     s = s.resample(resolution).mean().interpolate(method='linear')
