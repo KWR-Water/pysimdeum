5123fd0ffa7e99ff8ac7bce820e799b3ef9839c6
steffelbauer <david.steffelbauer@gmail.com>
2022-07-11 11:38:42 +0200

    added documentation for utils.py

--- pySIMDEUM/core/utils.py
+++ pySIMDEUM/core/utils.py
@@ -5,6 +5,15 @@ from dataclasses import dataclass
 
 
 def chooser(data: pd.Series | pd.DataFrame, myproperty: str=''):
+    """Function to choose elements from a pd.Series randomly, which consists of keys representing the elements and probabilities as values [-> Statistics object].
+
+    Args:
+        data (pd.Series | pd.DataFrame): input data to chose from which can be either a pandas.Series or a pandas.DataFrame
+        myproperty (str, optional): If the data is in form of a pandas.DataFrame then the myproperty property defines the column to chose from
+    Returns:
+        _type_: randomly chosen element from pandas.Series or pandas.DataFrame
+    """
+
     if not myproperty:
         data = pd.Series(data)
     else:
@@ -12,10 +21,19 @@ def chooser(data: pd.Series | pd.DataFrame, myproperty: str=''):
         types = data.keys()
         data = pd.Series(index=types,
                          data=[data[x][myproperty] for x in types])
+
+    # take only probabilities which are greater than 0
     data = data[data > 0]
+
+    # normalize probabilities to 0
     data /= (data.sum())
+
+    # choose a random number between 0 and 1 from a uniform distribution
     u = np.random.uniform()
+
+    # choose an element from the Series or DataFrame respectively randomly.
     choose = data[u < data.cumsum()].index[0]
+
     return choose
 
 
@@ -38,31 +56,54 @@ def duration_decorator(func):
     return wrapper
 
 
-def normalize(pdf):
+def normalize(pdf: np.ndarray | pd.Series) -> np.ndarray | pd.Series:
     """Normalize probability density function (pdf).
 
     Args:
-        pdf:
+        pdf (np.ndarray | pd.Series): probability density as numpy array or pandas.Series 
 
     Returns:
-
+        np.ndarray | pd.Series: normalized probability density
     """
     pdf = pdf / np.sum(pdf)
     return pdf
 
 
-def to_timedelta(value):
-    if isinstance(value, str):
-        value = pd.Timedelta(value)
-    elif isinstance(value, (int, float)):
+def to_timedelta(time: str | float | pd.Timedelta) -> pd.Timedelta:
+    """Transform a time in format string or float to a pandas.Timedelta object. 
+
+    The function raises an exception if the format is not a string, float or pandas.Timedelta object
+
+    Args:
+        time (str | float | pd.Timedelta): time in the format string, float or already pandas.Timedelta
+
+    Raises:
+        Exception: _description_
+
+    Returns:
+        pd.Timedelta: _description_
+    """
+
+    if isinstance(time, str):
+        value = pd.Timedelta(time)
+
+    elif isinstance(time, (int, float)):
         value = pd.Timedelta(seconds=value)
-    elif isinstance(value, pd.Timedelta):
-        pass
+
+    elif isinstance(time, pd.Timedelta):
+        value = time
+
     else:
-        raise Exception('Value is of unknoen type:', type(value))
+        raise Exception('Value is of unknown type:', type(value), '. It should be of type str, float, or pandas.Timedelta')
+
     return value
+
 
 @dataclass
 class Base:
+    """Base class of pySIMDEUM for generating objects. 
+    
+    The only argument of this parent class is an id which can either be set or will be set to a unique identifier
+    """
 
     id: str = str(uuid.uuid4())
