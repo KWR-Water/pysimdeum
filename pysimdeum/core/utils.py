ebfb8c323e0d824fe0afa205929644052443c26b
sophieoneill <106522632+sophieoneill@users.noreply.github.com>
2025-03-24 18:43:15 +0000

    Generalised utils functions

--- pysimdeum/core/utils.py
+++ pysimdeum/core/utils.py
@@ -428,6 +428,47 @@ def truncated_normal_dis_sampling(mean_value):
 
     return sample
 
+def discharge_postprocessing(ds, process_type, process_calculation):
+    """
+    Helper function to perform post-processing on discharge data. 
+    Called by assign_discharge_nutrients and assign_discharge_temperature.
+
+    Args:
+        ds (xarray.Dataset): The dataset containing discharge data and discharge events metadata.
+        process_type (str): The type of post-processing to perform.
+        calculation (function): The function to calculate nutrient concentrations.
+
+    Returns:
+        pd.DataFrame: The updated DataFrame containing the discharge data and the nutrient concentrations.
+    """
+
+    df, ref_start, ref_end = xarray_to_metadata_df(ds, 'discharge', 'discharge_events')
+
+    # group by event_label
+    grouped = df.groupby('event_label')
+
+    results = []
+
+    for event_label, event_data in grouped:
+        if pd.isna(event_label):
+            continue
+
+        # Compute total flow for the event
+        total_flow = event_data['flow'].sum()
+
+        # Process speific calculation
+        values = process_calculation(event_data, total_flow)
+
+        # Assign nutrient concentrations to the event
+        for value_name, value in values.items():
+            event_data[value_name] = value
+
+        results.append(event_data)
+
+    # Combine results back into a single DataFrame
+    df = pd.concat(results)
+
+    return df, ref_start, ref_end
 
 def assign_discharge_nutrients(ds):
     """Calculates nutrient concentrations based on simulated discharge flow data.
@@ -446,48 +487,76 @@ def assign_discharge_nutrients(ds):
     toml_file_path = os.path.join(DATA_DIR, 'NL', 'ww_nutrients.toml')
     nutrient_data = toml.load(toml_file_path)
 
-    df, ref_start, ref_end = xarray_to_metadata_df(ds, 'discharge', 'discharge_events')
-
+    def calculate_nutrients(event_data, total_flow):
     # list of nutrient types
-    nutrients = ['n', 'p', 'cod', 'bod5', 'ss', 'amm']
-    for nutrient in nutrients:
-        df[nutrient] = 0.0
+        nutrients = ['n', 'p', 'cod', 'bod5', 'ss', 'amm']
+        nutrient_values = {}
 
-    # group by event_label
-    grouped = df.groupby('event_label')
-
-    # Precompute nutrient concentrations for each event
-    nutrient_results = []
-    for event_label, event_data in grouped:
-        if pd.isna(event_label):
-            continue
-
-        # Extract metadata for the event
         enduse = event_data['enduse'].iloc[0]
         usage = event_data['usage'].iloc[0]
 
-        # Compute total flow for the event
-        total_flow = event_data['flow'].sum()
-
-        # Calculate nutrient concentrations for the event
-        nutrient_values = {}
         for nutrient in nutrients:
             mean_value = nutrient_data[enduse][usage][nutrient]
             nutrient_per_use = truncated_normal_dis_sampling(mean_value)
             nutrient_concentration = nutrient_per_use / total_flow if total_flow > 0 else 0
             nutrient_values[nutrient] = nutrient_concentration
 
-        # Assign nutrient concentrations to the event
-        for nutrient, value in nutrient_values.items():
-            event_data[nutrient] = value
+        return nutrient_values
 
-        nutrient_results.append(event_data)
-
-    # Combine results back into a single DataFrame
-    df = pd.concat(nutrient_results)
+    # Call the helper funcion
+    df, ref_start, ref_end = discharge_postprocessing(ds, 'nutrients', calculate_nutrients)
 
     return df, ref_start, ref_end
 
+def discharge_time_agg(df, time_agg='h'):
+    """
+    Helper function to aggregate the data by time based on the specified time aggregation level.
+
+    Args:
+        ds (xarray.Dataset): The dataset containing discharge data and discharge event metadata.
+        process_type (str): The type of post-processing to perform.
+        time_agg (str, optional): The time aggregation level. Options are:
+            - 's': Aggregate by seconds.
+            - 'm': Aggregate by minutes.
+            - '15min': Aggregate by 15-minute intervals.
+            - '30min': Aggregate by 30-minute intervals.
+            - 'h': Aggregate by hours (default).
+        process_calculation (function): The function to calculate nutrient concentrations or temperature.
+
+    Raises:
+        ValueError: If the input DataFrame does not contain the required columns ('time', 'flow', and nutrient types).
+        ValueError: If an invalid `time_agg` value is provided.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing the aggregated data with the following columns:
+            - 'time': The aggregated time intervals.
+            - 'flow': The total flow for each time interval.
+            - 'value_columns': Nutrient concentrations or temperature values.
+    """
+    # Round time to the specified aggregation level
+    if time_agg == 's':
+        df['agg_time'] = df['time']
+        freq = 'S'
+    elif time_agg == 'm':
+        df['agg_time'] = df['time'].dt.floor('min')  # Round to the nearest minute
+        freq = 'min'
+    elif time_agg == '15min':
+        df['agg_time'] = df['time'].dt.floor('15T')  # Round to the nearest 15 minutes
+        freq = '15T'
+    elif time_agg == '30min':
+        df['agg_time'] = df['time'].dt.floor('30T')  # Round to the nearest 30 minutes
+        freq = '30T'
+    elif time_agg == 'h':
+        df['agg_time'] = df['time'].dt.floor('h')  # Round to the nearest hour
+        freq = 'H'
+    else:
+        raise ValueError("Invalid time_agg value. Use 's' for seconds, 'm' for minutes, '15min' for 15mins, '30min' for 30mins, or 'h' for hours.")
+
+    # Group by date and the aggregated time
+    df['date'] = df['time'].dt.date
+    grouped = df.groupby(['date', 'agg_time'])
+
+    return df, grouped, freq
 
 def hh_discharge_nutrients(ds, time_agg='h'):
     """
@@ -516,7 +585,6 @@ def hh_discharge_nutrients(ds, time_agg='h'):
             - 'flow': The total flow for each time interval.
             - Nutrient columns (e.g., 'n', 'p', 'cod', 'bod5', 'ss', 'amm'): Nutrient concentrations.
     """
-
     df, ref_start, ref_end = assign_discharge_nutrients(ds)
 
     nutrients = ['n', 'p', 'cod', 'bod5', 'ss', 'amm']
@@ -525,28 +593,8 @@ def hh_discharge_nutrients(ds, time_agg='h'):
     if not all(col in df.columns for col in ['time', 'flow'] + nutrients):
         raise ValueError("Input DataFrame must contain columns for time, flow, and all nutrient types.")
 
-    # Round time to the specified aggregation level
-    if time_agg == 's':
-        df['agg_time'] = df['time']
-        freq = 'S'
-    elif time_agg == 'm':
-        df['agg_time'] = df['time'].dt.floor('min')  # Round to the nearest minute
-        freq = 'min'
-    elif time_agg == '15min':
-        df['agg_time'] = df['time'].dt.floor('15T')  # Round to the nearest 15 minutes
-        freq = '15T'
-    elif time_agg == '30min':
-        df['agg_time'] = df['time'].dt.floor('30T')  # Round to the nearest 30 minutes
-        freq = '30T'
-    elif time_agg == 'h':
-        df['agg_time'] = df['time'].dt.floor('h')  # Round to the nearest hour
-        freq = 'H'
-    else:
-        raise ValueError("Invalid time_agg value. Use 's' for seconds, 'm' for minutes, '15min' for 15mins, '30min' for 30mins, or 'h' for hours.")
-
-    # Group by date and the aggregated time
-    df['date'] = df['time'].dt.date
-    grouped = df.groupby(['date', 'agg_time'])
+    # time aggregation helper function
+    df, grouped, freq = discharge_time_agg(df, time_agg)
 
     # Group by time and calculate total flow
     flow = grouped['flow'].sum()
@@ -584,21 +632,7 @@ def assign_discharge_temperature(ds):
         pd.DataFrame: The updated DataFrame containing the discharge data and the temperatures.
 
     """
-
-    df, ref_start, ref_end = xarray_to_metadata_df(ds, 'discharge', 'discharge_events')
-
-    # group by event_label
-    grouped = df.groupby('event_label')
-
-    temp_results = []
-
-    for event_label, event_data in grouped:
-        if pd.isna(event_label):
-            continue
-
-        # Compute total flow for the event
-        total_flow = event_data['flow'].sum()
-
+    def calculate_temperature(event_data, total_flow):
         # Compute weighted average discharge temperature at the event
         if total_flow > 0:
             temperature_sum = (event_data['discharge_temperature'] * event_data['flow']).sum()
@@ -606,10 +640,9 @@ def assign_discharge_temperature(ds):
         else:
             avg_temperature = 0.0 # No flow
 
-        event_data['discharge_temperature'] = avg_temperature
-        temp_results.append(event_data)
+        return {'discharge_temperature': avg_temperature}
 
-    df = pd.concat(temp_results)
+    df, ref_start, ref_end = discharge_postprocessing(ds, 'temperature', calculate_temperature)
 
     return df, ref_start, ref_end
 
@@ -647,28 +680,8 @@ def hh_discharge_temperature(ds, time_agg='h'):
     if not all(col in df.columns for col in ['time', 'flow', 'discharge_temperature']):
         raise ValueError("Input DataFrame must contain columns for time, flow, and discharge_temperature.")
 
-    # Round time to the specified aggregation level
-    if time_agg == 's':
-        df['agg_time'] = df['time']
-        freq = 'S'
-    elif time_agg == 'm':
-        df['agg_time'] = df['time'].dt.floor('min')  # Round to the nearest minute
-        freq = 'min'
-    elif time_agg == '15min':
-        df['agg_time'] = df['time'].dt.floor('15T')  # Round to the nearest 15 minutes
-        freq = '15T'
-    elif time_agg == '30min':
-        df['agg_time'] = df['time'].dt.floor('30T')  # Round to the nearest 30 minutes
-        freq = '30T'
-    elif time_agg == 'h':
-        df['agg_time'] = df['time'].dt.floor('h')  # Round to the nearest hour
-        freq = 'H'
-    else:
-        raise ValueError("Invalid time_agg value. Use 's' for seconds, 'm' for minutes, '15min' for 15mins, '30min' for 30mins, or 'h' for hours.")
-    
-    # Group by date and the aggregated time
-    df['date'] = df['time'].dt.date
-    grouped = df.groupby(['date', 'agg_time'])
+    # time aggregation helper function
+    df, grouped, freq = discharge_time_agg(df, time_agg)
 
     # Group by time and calculate total flow
     flow = grouped['flow'].sum()    
